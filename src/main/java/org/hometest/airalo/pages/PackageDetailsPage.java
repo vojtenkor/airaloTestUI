19269f9e83e5497f8f86b8414c3c8abd33cf0a18
Romchik <vojtenkor@gmail.com>
2024-07-31 14:22:02 +0300

    Fixed locator for sim card data value

--- src/main/java/org/hometest/airalo/pages/PackageDetailsPage.java
+++ src/main/java/org/hometest/airalo/pages/PackageDetailsPage.java
@@ -7,7 +7,7 @@ public class PackageDetailsPage extends AbstractPage {
     private final By packageNameFieldLocator = By.xpath("//div[@data-testid='package-detail']");
     private final By simDetailOperatorTitleTextLocator = By.xpath("//div[@data-testid='sim-detail-operator-title']/p");
     private final By coverageValueLocator = By.xpath("//p[@data-testid='COVERAGE-value']");
-    private final By dataValueLocator = By.xpath("//p[@data-testid='DATA-value']");
+    private final By dataValueLocator = By.xpath("//div[@data-testid='sim-detail-header']//p[@data-testid='DATA-value']");
     private final By validityValueLocator = By.xpath("//div[@data-testid='sim-detail-header']//p[@data-testid='VALIDITY-value']");
     private final By priceValueLocator = By.xpath("//div[@data-testid='sim-detail-header']//p[@data-testid='PRICE-value']");
     
